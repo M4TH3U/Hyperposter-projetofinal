2df278caad23d8ea2f89e3b7b2821d22362477d7
M4TH3U <108372676+M4TH3U@users.noreply.github.com>
2022-07-19 21:01:39 -0300

    Update form_add_login.php

--- src/View/form_add_login.php
+++ src/View/form_add_login.php
@@ -5,18 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,
     initial-scale=1.0">
-    <link rel="stylesheet" href="src/css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>hyperposters</title>
 
 <body>
     <header> 
         <nav>
-            <section id="logo"><img src="./image/logo.png" alt=""></section>
+            <section id="logo"><img src="../image/logo.png" alt=""></section>
                 <article id="icones">
-                <img src="./image/pesquisar.png" alt=""></img>
-                <img src=".src/image/carrinho.png" alt="">
-                <a href="src/View/form_add_login.php"><img src="./image/perfil.png" alt=""></a>
+                <img src="../image/pesquisar.png" alt=""></img>
+                <img src="../image/carrinho.png" alt="">
+                <img src="../image/perfil.png" alt=""></a>
             </article>
         </nav>
     </header>
+    <section>
+    <h1 class="Acessar-conta">Acessar conta</h1>
+    <div class="login">
+        <h1 id="login">Login</h1>
+        <input id="email" type="text" placeholder="E-mail">
+        <br><br>
+        <input id="senha" type="password" placeholder="senha">
+        <br><br>
+        <button id="botao">Entrar</button>
+        <h1 id="cadastrar-se">Ainda não tem uma conta?Cadastre-se</h1>
+    </div>
+</section>
 </body>
